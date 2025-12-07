4ed24d46a108302a951d4835f81192501b2ee2df
malakzaidi <malak.zaidi-etu@etu.univh2c.ma>
2025-12-07 15:18:44 +0100

    initialize the project

---
+++ src/main/java/org/spring/democqrsaxon/DemoCqrsAxonApplication.java
@@ -0,0 +1,13 @@
+package org.spring.democqrsaxon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCqrsAxonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCqrsAxonApplication.class, args);
+    }
+
+}
